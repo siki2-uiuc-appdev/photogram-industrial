0b3055d5192b54f473c24817b030e072a3912db2
siki2-uiuc <siki2@illinois.edu>
2022-11-28 17:40:35 +0000

    Generated photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_28_162745) do
+ActiveRecord::Schema.define(version: 2022_11_28_172322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
+    t.text "caption"
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_photos_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.citext "email", default: "", null: false
@@ -33,4 +44,5 @@ ActiveRecord::Schema.define(version: 2022_11_28_162745) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "photos", "users", column: "owner_id"
 end
