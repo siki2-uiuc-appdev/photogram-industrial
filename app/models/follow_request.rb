0f599915eb50ca931bab664f95834fb686c4fb5c
siki2-uiuc <siki2@illinois.edu>
2022-11-28 20:56:02 +0000

    Added auto annotate

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: follow_requests
+#
+#  id           :bigint           not null, primary key
+#  recipient_id :bigint           not null
+#  sender_id    :bigint           not null
+#  status       :string           default("pending")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
