04a7802008a00271a39fac2c907de8b14f209e31
siki2-uiuc <siki2@illinois.edu>
2022-11-28 21:01:56 +0000

    Added counter caches

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,23 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
-#  author_id  :bigint           not null
-#  photo_id   :bigint           not null
 #  body       :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  author_id  :bigint           not null
+#  photo_id   :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_photo_id   (photo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (photo_id => photos.id)
 #
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :photo
+  belongs_to :author, class_name: "User", counter_cache: true
+  belongs_to :photo, counter_cache: true
 end
---
+++ db/migrate/20221128205925_add_photo_count_to_users.rb
@@ -0,0 +1,5 @@
+class AddPhotoCountToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :photos_count, :integer, default: 0
+  end
+end
