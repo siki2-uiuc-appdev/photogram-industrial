9a3e0bd8abaadf5a80d08756ca7517593866cc3d
siki2-uiuc <siki2@illinois.edu>
2022-11-28 18:02:55 +0000

    Generated follow requests and associations with follow requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
 
   has_many :comments, foreign_key: "author_id"
+
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
+
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
 end
