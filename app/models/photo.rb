d85b42e1110c2ee16300b4493196c2662e38aa1d
siki2-uiuc <siki2@illinois.edu>
2022-11-28 18:15:10 +0000

    Added direct associations of likes to users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
   has_many :comments
+
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
 
+  has_many :liked_photos, class_name: "Like", foreign_key: "fan_id"
+
   has_many :comments, foreign_key: "author_id"
 
   has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
