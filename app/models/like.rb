a145868f99c1549ffba43297e596d3ee7fc67df4
siki2-uiuc <siki2@illinois.edu>
2022-11-29 17:38:41 +0000

    Created sample data file

--- app/models/like.rb
+++ app/models/like.rb
@@ -19,6 +19,8 @@
 #  fk_rails_...  (photo_id => photos.id)
 #
 class Like < ApplicationRecord
+  validates :user_id, uniqueness: { scope: :photo_id, message: "has already liked this photo" }
+  
   belongs_to :fan, class_name: "User", counter_cache: true
   belongs_to :photo, counter_cache: true
 end
