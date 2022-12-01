8c591ea773d7e209d6c7a0c3423b3baa4bb5c99e
siki2-uiuc <siki2@illinois.edu>
2022-12-01 18:38:56 +0000

    Added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
 
   # get "users/:id" => "users#show", as: user
 
-  
   devise_for :users
   
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
   
+  # Has to be last route because it's the most general
+  get "/:username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
