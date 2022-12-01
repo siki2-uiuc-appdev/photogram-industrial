8d62b411fda67d32fa26aa1e4ab3e00670aad3f7
siki2-uiuc <siki2@illinois.edu>
2022-12-01 17:49:17 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  devise_for :users
+  # get "users/:id" => "users#show", as: user
 
+  
+  devise_for :users
+  
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+  resources :users, only: :show
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
